2e78f267ce9c2232e2ee98503e89d508b37b9b11
Adityavijayvargiya01 <adityavijayvargiya2022@gmail.com>
2024-06-03 22:32:59 +0530

    Update button label in UserNav component

--- app/components/UserNav.tsx
+++ app/components/UserNav.tsx
@@ -46,7 +46,7 @@ export async function UserNav() {
             <DropdownMenuItem>
               <form action={createHomewithId} className="w-full">
                 <button type="submit" className="w-full text-start">
-                  Airbnb your Home
+                  Your Uploads
                 </button>
               </form>
             </DropdownMenuItem>
